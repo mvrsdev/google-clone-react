178b5796b485bbed3ebc4a3b0506090a745a73e0
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-06-23 21:29:26 -0300

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import Button from './Button';
+
+const HeaderRow = styled.div`
+  display: flex;
+  height: 60px;
+  justify-content: flex-end;
+  color: #000000de;
+  align-items: center;
+  a {
+    padding: 15px;
+    line-height: 24px;
+    font-size: 14px;
+    cursor: pointer;
+    color: #000000de;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  #login-button {
+  background-color: #1a73e8;
+  border: none;
+  color: white;
+  padding: 9px 23px;
+  font-weight: 500;
+  letter-spacing: 0.25px;
+  line-height: 16px;
+  margin-left: 10px;
+  margin-right: 8px;
+  font-size: 14px;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 4px;
+  -webkit-border-radius: 4px;
+  &:hover {
+    background: #4285f4;
+  }
+}
+`;
+
+const Header = ({ isVisibleGmailImage = true }) => {
+  const renderGmailImage = () => (
+    <>
+      <a href="index.com">Gmail</a>
+      <a href="index.com">Imagens</a>
+    </>
+  );
+  const renderSpan = () => <span>settings</span>;
+  return (
+    <HeaderRow>
+      {isVisibleGmailImage ? renderGmailImage() : renderSpan()}
+      <button id="login-button">Fazer login</button>
+    </HeaderRow>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as IconButton } from './IconButton';
 export { default as SearchInputField } from './SearchInputField';
-
+export { default as Header} from './Header';
