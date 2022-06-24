86e4f051dd75feb6d21030fefee4b41813a8bb66
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-06-24 18:56:31 -0300

    Add HomePage main area

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,44 @@
-import { Header } from 'components';
+import { Button, Header, SearchInputField } from 'components';
+import styled from 'styled-components';
 
+const MainRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 84vh;
+  align-items: center;
+  justify-content: center;
+
+  .SearchInputFieldWrapper {
+    width: 45vw;
+    padding-top: 40px;
+    padding-bottom: 16px;
+  }
+
+  fieldset {
+    border: none;
+    display: flex;
+    justify-content: center;
+  }
+
+  Button {
+    margin: 11px;
+  }
+`;
 
 const HomePage = () => (
-  < Header isVisibleGmailImage={true}/>
+  <>
+    <Header isVisibleGmailImage={true} />
+    <MainRow>
+      <img src="images/google-logo.svg" alt="Logo do Google" />
+      <div className="SearchInputFieldWrapper">
+        <SearchInputField />
+      </div>
+      <fieldset>
+        <Button>Pesquisa Google</Button>
+        <Button>Estou com sorte</Button>
+      </fieldset>
+    </MainRow>
+  </>
 );
-
 
 export default HomePage;
